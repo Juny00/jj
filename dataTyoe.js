8972c6b86c7a30ab6b219e6041b3eeb252a8882c
Juny00 <127288510+Juny00@users.noreply.github.com>
2023-03-10 17:31:03 +0900

    Create dataTyoe.js

---
+++ dataTyoe.js
@@ -0,0 +1,32 @@
+//8종류의 데이터 타입
+//String, Number, Bigint, Boolean, Undefined, Null, Symbol, Object
+
+//String ** 문자멸
+const st1 = "smith";
+const st2 = "1";
+const st3 = "true";
+console.log(num1, typeof num1)
+
+//Number **
+const num1 = 1;
+const num2 = 12341234;
+const num3 = 1.5;
+
+//Boolean **
+const bool1 = true;
+const bool2 = false;
+console.log(bool, typeof bool1);
+
+//Object **
+const obj1 = [1,2,3,4,5,6,7, "Smith"]; //배열, Array
+const obj2 = {
+    userName: "Smith",
+    userAge: 25
+};
+
+//Undefined
+let undef; //const 상수 상수는 반드시 값이 있어야기에 같이 못씀
+
+//Null
+let nulValue = null;
+//의미는 없지만 넬셋팅을 해준다
