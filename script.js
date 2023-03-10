c43e770f9f76c1fe5265dff2ee581e2063a7d0d5
Juny00 <127288510+Juny00@users.noreply.github.com>
2023-03-10 17:31:14 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("demo").innerHTML = "Hello World!!!!";
+//console.log 
+//console.log(document.getElementById("demo"));
+//console.log(document.getElementById("demo").innerHTML);
+//document.getElementById("demo").innerHTML = "1111111111";
+//console.log(document.getElementById("demo"));
+//console.log(document.getElementById("demo").innerHTML);
+//document.getElementById("demo").innerHTML = "222222222222";
